2c8e3d93892020eaf67ac3d5baa11ebe6ebbfd7b
Joshua Richards <joshua.j.richards@gmail.com>
2020-05-24 10:56:49 -0400

    try methodology build

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,53 @@ module.exports = {
   /* Your site config here */
   siteMetadata: {
     title: `Bail Reform Project`,
-    author: 'Law & Design CoLab'
-  },
+    author: 'Law & Design CoLab',
+  
+  methodology : [
+    {
+      year: "2011",
+      headline : "Lorem Ipsum",
+      copy : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris scelerisque orci nec tellus elementum fermentum. Nulla vestibulum libero est, sodales condimentum enim lacinia in. Duis neque ipsum, commodo sit amet leo et, dignissim volutpat sem.",
+      docs : [
+        {
+          date: "some-date",
+          tags: [ "mental health", "some tag"],
+          author: "Nicole Myers",
+          title: "Creating Criminality: The Intensification of Institutional Risk Aversion Strategies and the Decline of the Bail Process",
+          quote: "Direct quote from the document goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        },
+        {
+          date: "another-date",
+          tags: [ "mental health", "another tag"],
+          author: "another author",
+          title: "another title",
+          quote: "another quote"
+        }
+      ]
+    },
+    {
+      year: "2012",
+      headline : "Lorem Ipsum",
+      copy : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris scelerisque orci nec tellus elementum fermentum. Nulla vestibulum libero est, sodales condimentum enim lacinia in. Duis neque ipsum, commodo sit amet leo et, dignissim volutpat sem.",
+      docs : [
+        {
+          date: "some-date",
+          tags: [ "mental health", "some tag"],
+          author: "some author",
+          title: "some title",
+          quote: "some quote"
+        },
+        {
+          date: "another-date",
+          tags: [ "mental health", "another tag"],
+          author: "another author",
+          title: "another title",
+          quote: "another quote"
+        }
+      ]
+    }
+  ],
+},
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
--- src/pages/methodology.js
+++ src/pages/methodology.js
@@ -3,27 +3,31 @@ import Layout from "../components/layout"
 import Head from '../components/head';
 
 import { Link, graphql, useStaticQuery } from "gatsby"
-import { Container, Card } from "react-bootstrap"
+import { Container, Card, Row, Col, Button } from "react-bootstrap"
 
 // import blogStyles from "./blog.module.scss"
 
 
 const MethodologyPage = () => {
+  
+  function indicatorClickHandler() {
+    console.log('clicked');
+  }
+
   const data = useStaticQuery(graphql`
     query {
-      allContentfulBlogPost(
-        sort:{
-          fields: publishedDate,
-          order:DESC
-        }
-      ){
-        edges {
-          node {
-            title
-            slug
-            publishedDate (
-              formatString: "MMMM Do, YYYY"
-            )
+      site {
+        siteMetadata {
+          methodology {
+            year
+            headline
+            copy
+            docs {
+              date
+              author
+              title
+              quote
+            }
           }
         }
       }
@@ -33,21 +37,65 @@ const MethodologyPage = () => {
   return (
     <Layout>
       <Head title="Methodology"/>
-      <Container className="mt-4">
-      <h1>Methodology</h1> 
-      <p>Posts will show up heree.</p>
-      { data.allContentfulBlogPost.edges.map(edge => (
-        <Link key={edge.node.slug} to={ `/blog/${edge.node.slug}`}>
-          <Card className="mb-2">
-            <Card.Body>
-              <h2>{edge.node.title}</h2>
-              <p className="mb-0">{edge.node.publishedDate}</p>
-            </Card.Body>
-          </Card>
-        </Link>
-      ))}
-      </Container>
+      <div className="container">
+        <Row className="">
+          <Col md="4" xl="3" className="p-4 p-xl-5">
+            <h2>interaction</h2>
+          </Col>
+          <Col md="8" xl="9" className="h-100 p-4 p-xl-5">
+            <div className="timeline-wrapper mr-5">
+            { data.site.siteMetadata.methodology.map(item => (
+              <div key={item.year} className="timeline-year mb-5">
+                <div className="timeline-year-content">
+                  <div className="timeline-year-content-header d-flex pb-2 mb-4">
+                    <h1 className="display-3 pr-3"><strong>{item.year}</strong></h1>
+                    <div className="timeline-year-header-meta mt-2 pr-5 pb-3">
+                      <p className="mb-1"><strong>{item.headline}</strong></p>
+                      <p className="mb-0">{item.copy}</p>
+                    </div>
+                    { item.docs.map((doc, index) => (
+                      <div key={index} className="timeline-card-indicator bg-primary" id={`${item.year}-card-${index}`} style={{ left: index*40 + 'px'}} onClick={ indicatorClickHandler }>{index}</div>
+                    ))}
+                  </div>
+
+                  { item.docs.map((doc, index) => (
+                    <Card key={`card-${index}`}className="w-75" id={`${item.year}-card-${index}`} >
+                      <Card.Header className="text-right bg-primary text-white">
+                        <small>{doc.date}</small>
+                      </Card.Header>
+                      <Card.Body>
+                        <p>{doc.author}</p>
+                        <p>{doc.title}</p>
+                        <p>{doc.quote}</p>
+                        <Button className="btn-dark">View Document</Button>
+                      </Card.Body>
+                    </Card>
+                  )) }
+                </div>
+              </div>
+            ))}
+            </div>
+          </Col>
+        </Row>
+      </div>
     </Layout>
+    // <Layout>
+    //   <Head title="Methodology"/>
+    //   <Container className="mt-4">
+    //   <h1>Methodology</h1> 
+    //   <p>Posts will show up heree.</p>
+    //   { data.allContentfulBlogPost.edges.map(edge => (
+    //     <Link key={edge.node.slug} to={ `/blog/${edge.node.slug}`}>
+    //       <Card className="mb-2">
+    //         <Card.Body>
+    //           <h2>{edge.node.title}</h2>
+    //           <p className="mb-0">{edge.node.publishedDate}</p>
+    //         </Card.Body>
+    //       </Card>
+    //     </Link>
+    //   ))}
+    //   </Container>
+    // </Layout>
   )
 }
 
