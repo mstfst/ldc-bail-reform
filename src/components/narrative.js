196029ea2456c9f2b6f3239539c8153bfa24f1b7
Donn Pasiliao <dpasiliao@gmail.com>
2020-05-27 09:26:16 -0400

    Add files via upload

---
+++ src/components/narrative.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Layout from "../components/layout"
+import Head from '../components/head';
+import DocumentCard from '../components/document-card';
+
+import { graphql, StaticQuery } from "gatsby"
+import { Row, Col } from "react-bootstrap"
+
+class NarrativeSection extends Component {
+  render () {
+  return (
+    <div>
+      Narrative Component
+    </div>
+  )
+}
+}
+
+export default NarrativeSection
