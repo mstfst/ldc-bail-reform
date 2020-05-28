b8f42d81abcc33fe31c31ea39f28c8bece6d7f17
Donn Pasiliao <dpasiliao@gmail.com>
2020-05-28 07:09:10 -0400

    added fade effect in narrative

--- src/components/narrative-body.js
+++ src/components/narrative-body.js
@@ -9,13 +9,18 @@ import { graphql, StaticQuery } from "gatsby"
 import { Row, Col } from "react-bootstrap"
 
 
-
+const narration = [
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec risus urna, tincidunt vitae lacus ut, aliquet pellentesque justo. Duis vel ante tincidunt, tincidunt ipsum et, placerat dui. Fusce at sem at ipsum faucibus auctor. Nam sapien nisi, auctor imperdiet suscipit sit amet, dignissim at libero. In in lorem facilisis, egestas enim a, vestibulum lorem. Aenean facilisis leo id justo pharetra volutpat. Vivamus consequat iaculis ultricies. Suspendisse eu tincidunt odio, vitae aliquam velit. Integer tristique metus mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis rutrum molestie metus vel sodales. Cras a massa non justo bibendum egestas a nec lectus. Fusce faucibus sapien vel nulla dapibus ornare. Nulla fringilla felis non venenatis feugiat. Nam ultricies ornare sapien eu luctus.",
+  "Aenean interdum laoreet massa, a scelerisque metus vestibulum eget. Fusce libero eros, egestas ac gravida at, tincidunt non libero. Sed tempus ullamcorper augue. Nullam accumsan odio risus, ut maximus mi gravida consequat. In blandit nibh vel dui ultricies maximus. Vivamus fermentum et lacus at aliquet. Nullam et neque faucibus, pellentesque erat sed, vulputate nisi. Mauris velit justo, aliquet eget laoreet imperdiet, cursus quis enim. Etiam viverra nunc dapibus felis elementum placerat. In et sollicitudin odio, sagittis tincidunt tortor. In eget quam bibendum, pretium tellus ac, ultricies elit. Integer rutrum, massa ac iaculis facilisis, ante risus auctor elit, nec tempor arcu sem id magna. Sed venenatis augue mauris, fringilla laoreet sapien maximus rutrum. Sed pretium, mauris eget egestas porta, lorem urna accumsan nisi, vitae facilisis tortor odio sit amet tellus. Vivamus varius fermentum libero. Cras nibh lectus, consectetur sit amet luctus ut, fringilla ornare quam.",
+  "Nullam vehicula libero vel augue fermentum, ut ultrices felis mollis. Duis a arcu lacus. Donec a urna non metus posuere luctus eget et risus. Praesent diam turpis, finibus eu dolor eu, cursus scelerisque est. Morbi fringilla feugiat urna malesuada ultrices. Cras vitae placerat erat, non finibus diam. Curabitur vitae mi ex. Donec in convallis mauris."
+]
 
 class NarrativeSection extends Component {
   scroller;
   
   state = {
-    data: 0,
+    stp: 0,
+    steps: [0,1,2],
     backgroundImages: ["assets/cell-window.jpg", "assets/convict-direction.jpg", "assets/jail-cell.jpg"],
     progress: 0,
     narrativeBgImg: "assets/cell-window.jpg"
@@ -23,10 +28,13 @@ class NarrativeSection extends Component {
 
   handleScrollStepEnter = ({element, index, direction}) => {
     // element.style.backgroundColor = 'lightgoldenrodyellow';
+    this.setState({
+      stp: this.state.steps[index]
+    }) 
   }
   handleScrollStepExit = ({element, index, direction}) => {
     // element.style.backgroundColor = 'white';
-    console.log("scroll step exit")
+    // console.log("scroll step exit")
   }
 
   handleProgress = ({progress}) => {
@@ -54,7 +62,7 @@ class NarrativeSection extends Component {
 
     this.scroller.setup({
       container: '#narrative-scroll',
-      step: '#narrative-step',
+      step: '.narrative-step',
       threshold: scrollThreshold,
       progress: true,
       debug: false
@@ -72,7 +80,7 @@ class NarrativeSection extends Component {
   }
 
   render () {
-  const { data, steps, progress, images, narrativeBgImg } = this.state;
+  const { stp, steps, progress, images, narrativeBgImg } = this.state;
 
   const narrativePgStyle = {
     paddingTop: "100vh",
@@ -95,7 +103,7 @@ class NarrativeSection extends Component {
         backgroundSize: "cover",
         position: "fixed",
         zIndex: "-1",
-        opacity: progress < 0.3? 1 : 0
+        opacity: 1
       }}>
       </div>
       <div style={{
@@ -108,7 +116,7 @@ class NarrativeSection extends Component {
         backgroundSize: "cover",
         position: "fixed",
         zIndex: "-1",
-        opacity: progress > 0.3 & progress < 0.66 ? 1 : 0
+        opacity: stp === 1? progress*2-0.2 : stp === 2? 1 : 0
       }}>
       </div>
       <div style={{
@@ -121,26 +129,17 @@ class NarrativeSection extends Component {
         position: "fixed",
         backgroundSize: "cover",
         zIndex: "-1",
-        opacity:  progress > 0.66? 1 : 0
+        opacity:  stp === 2? progress*2-0.2 : 0
       }}>
       </div>
     <div id="narrative-scroll">
+      {narration.map( (page, pageNum) => (
+        <div className="narrative-step" style={narrativePgStyle}>
+          {page}
+          {console.log(stp)}
+        </div>
+      ))}
       
-      <div id="narrative-step" style={{
-        height:"600vh"
-        }}>
-        <div style={narrativePgStyle}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec risus urna, tincidunt vitae lacus ut, aliquet pellentesque justo. Duis vel ante tincidunt, tincidunt ipsum et, placerat dui. Fusce at sem at ipsum faucibus auctor. Nam sapien nisi, auctor imperdiet suscipit sit amet, dignissim at libero. In in lorem facilisis, egestas enim a, vestibulum lorem. Aenean facilisis leo id justo pharetra volutpat. Vivamus consequat iaculis ultricies. Suspendisse eu tincidunt odio, vitae aliquam velit. Integer tristique metus mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis rutrum molestie metus vel sodales. Cras a massa non justo bibendum egestas a nec lectus. Fusce faucibus sapien vel nulla dapibus ornare. Nulla fringilla felis non venenatis feugiat. Nam ultricies ornare sapien eu luctus.
-        </div>
-
-        <div style={narrativePgStyle}>
-          Aenean interdum laoreet massa, a scelerisque metus vestibulum eget. Fusce libero eros, egestas ac gravida at, tincidunt non libero. Sed tempus ullamcorper augue. Nullam accumsan odio risus, ut maximus mi gravida consequat. In blandit nibh vel dui ultricies maximus. Vivamus fermentum et lacus at aliquet. Nullam et neque faucibus, pellentesque erat sed, vulputate nisi. Mauris velit justo, aliquet eget laoreet imperdiet, cursus quis enim. Etiam viverra nunc dapibus felis elementum placerat. In et sollicitudin odio, sagittis tincidunt tortor. In eget quam bibendum, pretium tellus ac, ultricies elit. Integer rutrum, massa ac iaculis facilisis, ante risus auctor elit, nec tempor arcu sem id magna. Sed venenatis augue mauris, fringilla laoreet sapien maximus rutrum. Sed pretium, mauris eget egestas porta, lorem urna accumsan nisi, vitae facilisis tortor odio sit amet tellus. Vivamus varius fermentum libero. Cras nibh lectus, consectetur sit amet luctus ut, fringilla ornare quam.
-        </div>
-
-        <div style={narrativePgStyle}>
-          Nullam vehicula libero vel augue fermentum, ut ultrices felis mollis. Duis a arcu lacus. Donec a urna non metus posuere luctus eget et risus. Praesent diam turpis, finibus eu dolor eu, cursus scelerisque est. Morbi fringilla feugiat urna malesuada ultrices. Cras vitae placerat erat, non finibus diam. Curabitur vitae mi ex. Donec in convallis mauris.
-        </div>
-      </div>
     </div>
     </div>
     
