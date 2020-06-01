99621e74caa404346b9bb836adfc49fe07f13f91
Andrea Giambelli <andreagiambelli@hotmail.it>
2020-06-01 17:02:05 -0400

    tooltip test

--- src/pages/system-map.js
+++ src/pages/system-map.js
@@ -15,18 +15,18 @@ class SystemMapPage extends Component {
   handleScrollStepEnter = ({ element, index, direction }) => {
     console.log("enter")
     if (index === 0) {
-      D3.select("#cogs").style("opacity", 0);
-      D3.select("#zaps").style("opacity", 0);
+      D3.select("#cogs").style("opacity", 0)
+      D3.select("#zaps").style("opacity", 0)
     }
 
     if (index === 1) {
-      D3.select("#cogs").style("opacity", 1);
-      D3.select("#zaps").style("opacity", 0);
+      D3.select("#cogs").style("opacity", 1)
+      D3.select("#zaps").style("opacity", 0)
     }
 
     if (index === 2) {
-      D3.select("#cogs").style("opacity", 0);
-      D3.select("#zaps").style("opacity", 1);
+      D3.select("#cogs").style("opacity", 0)
+      D3.select("#zaps").style("opacity", 1)
     }
   }
 
@@ -41,10 +41,10 @@ class SystemMapPage extends Component {
     console.log(this.steps)
     console.log(this.systemMap)
 
-    let stepH = Math.floor(window.innerHeight * 0.75);
-    this.steps.style("height", window.innerHeight * 0.8 + "px");
+    let stepH = Math.floor(window.innerHeight * 0.75)
+    this.steps.style("height", window.innerHeight * 1.5 + "px")
 
-    this.scroller.resize();
+    this.scroller.resize()
   }
 
   componentDidMount() {
@@ -52,8 +52,7 @@ class SystemMapPage extends Component {
     this.systemMap = D3.select("#system-map")
     this.steps = D3.select("#step-wrapper").selectAll(".step")
 
-    
-// Creating the scroller
+    // Creating the scroller
     const scrollama = require("scrollama")
     this.scroller = scrollama()
 
@@ -79,26 +78,46 @@ class SystemMapPage extends Component {
     // window.addEventListener("resize", this.handleResize);
 
     // Loading the Systemp Map svg
-    D3.xml("assets/svg/systemMap.svg").then(function (smSvg) {
+    D3.xml("assets/svg/SM_mag31.svg").then(function (smSvg) {
       const viewBoxWidth = 1400 // svg container width
-      const viewBoxHeight = 600 // svg container height
+      const viewBoxHeight = 700 // svg container height. Needs to be the same as height for svg-wrapper specified in SCSS
 
       // Storing a selection of the root node for the imported SVG
       let svgMap = D3.select(smSvg).select("svg").node()
 
       // Appending the imported SVG to svg-wrapper
-      let mainSvg = D3.select("#svg-wrapper")
-        .append("svg")
+      let mainSvg = D3.select("#svg-wrapper").node().appendChild(svgMap)
+
+      D3.select(svgMap)
         .attr("width", "100%")
         .attr("height", "100%")
         .attr("viewBox", "0 0 " + viewBoxWidth + " " + viewBoxHeight)
-        .node()
-        .appendChild(svgMap)
 
       // Hiding elements that shouldn't appear right away
       // These will later be shown based on scrollama triggers
       D3.select("#zaps").style("opacity", 0)
       D3.select("#cogs").style("opacity", 0)
+
+      let style = window.getComputedStyle(D3.select(".mt-4.container").node()).marginLeft
+      console.log(style)
+
+      // console.log(D3.select(".mt-4.container").node())
+      D3.select("#zaps")
+        .selectAll("rect")
+        .on("click", function (d, i, n) {
+          console.log(d)
+          console.log(i)
+          console.log(n)
+          console.log(D3.event.pageX)
+          console.log(D3.event.pageY)
+          D3.select("#tooltip-div")
+            .style("left", D3.event.pageX -150 + "px")
+            .style("top", D3.event.pageY - 28 + "px")
+            .style("opacity", "1")
+            .style("pointer-events", "auto")
+
+          // D3.select("body").style("overflow", "hidden")
+        })
     })
   }
 
@@ -115,14 +134,20 @@ class SystemMapPage extends Component {
           <p>D3 will load here.</p>
 
           <div id="system-map">
-            {/* <svg id="svg-wrapper" style={divStyle}></svg> */}
-            <svg id="svg-wrapper"></svg>
+          <div id="tooltip-wrapper">
+              <div id="tooltip-div">
+                <p>Tooltip</p>
+                <button id="closeTooltip">X</button>
+              </div>
+            </div>
+            <div id="svg-wrapper"></div>
             <div id="step-wrapper">
               <div className="step"></div>
               <div className="step"></div>
               <div className="step"></div>
               <div className="step"></div>
             </div>
+            
           </div>
         </Container>
       </Layout>
