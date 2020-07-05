8b1b8429266cdb07d8739d442028e63347e58233
Maggie <mc@maggiecheung.ca>
2020-07-05 16:01:29 -0400

    Removed GraphQL query since it wasn't being used in this component

--- src/components/header.js
+++ src/components/header.js
@@ -7,21 +7,12 @@ import headerStyles from './header.module.scss';
 import Logo from "../../static/assets/svg/logo_navbar.svg";
 
 const Header = () => {
-  const data = useStaticQuery(graphql`	
-    query {	
-      site {	
-        siteMetadata {	
-          title	
-        }	
-      }	
-    }	
-  `);	
-
+  
   return (
     <Navbar collapseOnSelect expand="md" bg="dark" variant="dark" fixed="top">
       <Container>
         <Navbar.Brand as={Link} to="/" aria-label="Go to homepage">
-          <Logo width={200} alt={data.site.title} />
+          <Logo width={200}/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" className={headerStyles.nav}>
