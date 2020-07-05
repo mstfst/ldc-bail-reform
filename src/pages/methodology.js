f10159102642de2ce4cfd8542355757e7215df04
Joshua Richards <joshua.j.richards@gmail.com>
2020-07-05 12:13:46 -0400

    get timeline data into page

--- src/pages/methodology.js
+++ src/pages/methodology.js
@@ -1,174 +1,174 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Layout from "../components/layout"
 import Head from '../components/head';
 import DocumentCard from '../components/document-card';
 
-import { graphql, StaticQuery } from "gatsby"
-import { Row, Col } from "react-bootstrap"
+import { graphql, useStaticQuery } from "gatsby"
+import { Container, Row, Col } from "react-bootstrap"
 
-class MethodologyPage extends Component {
-  scroller;
+const MethodologyPage = () => {
+  // scroller
 
-  state = {
-    data: 0,
-    steps: [10, 20, 30],
-    progress: 0,
-    documents: {
-      '2011-card-0': true,
+  // const state = {
+  //   data: 0,
+  //   steps: [10, 20, 30],
+  //   progress: 0,
+  //   documents: {
+  //     '2011-card-0': true,
+  //   }
+  // }
+
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulTimelineYear {
+        edges {
+          node {
+            year
+            headline
+            description {
+              description
+            }
+          }
+        }
+      }
+      allContentfulTimelineEvent {
+        edges {
+          node {
+            eventDate
+            eventTitle
+          }
+        }
+      }
+      allContentfulTimelineDocument {
+        edges {
+          node {
+            title
+            date
+            author
+            quote
+            url
+          }
+        }
+      }
     }
-  }
+  `)
 
-  handleScrollStepEnter = ({element, index, direction}) => {
+  const years = data.allContentfulTimelineYear.edges;
+  const documents = data.allContentfulTimelineEvent.edges;
+  const events = data.allContentfulTimelineDocument.edges;
 
-    console.log(element.dataset.index);
+  console.log(years);
+  console.log(documents);
+  console.log(events);
 
-    const data = this.state.steps[index];
-    element.classList.add('active');
-    this.setState({data});
+  // const handleScrollStepEnter = ({element, index, direction}) => {
 
-    this.updateActiveDocumentCard(`${element.dataset.index}-card-0`);
-  }
+  //   console.log(element.dataset.index);
 
-  handleScrollStepExit = ({element, index, direction}) => {
-    element.classList.remove('active');
-  }
+  //   const data = this.state.steps[index];
+  //   element.classList.add('active');
+  //   this.setState({data});
 
-  handleProgress = ({progress}) => {
-    this.setState({progress});
-  }
+  //   this.updateActiveDocumentCard(`${element.dataset.index}-card-0`);
+  // }
 
-  componentDidMount(){
-    const scrollama = require('scrollama')
-    const scrollThreshold = 0.2;
-    this.scroller = scrollama()
-    this.scroller.setup({
-      step: '.timeline-year',
-      threshold: scrollThreshold,
-      progress: true,
-      // debug: true
-    })
-    .onStepEnter(this.handleScrollStepEnter)
-    .onStepExit(this.handleScrollStepExit)
-    .onStepProgress(this.handleProgress)
+  // const handleScrollStepExit = ({element, index, direction}) => {
+  //   element.classList.remove('active');
+  // }
 
-    // setup resize event
-    window.addEventListener("resize", this.scroller.resize);
-  }
+  // const handleProgress = ({progress}) => {
+  //   this.setState({progress});
+  // }
 
-  componentWillUnmount(){
-    this.scroller.destroy();
-  }
+  // componentDidMount(){
+  //   const scrollama = require('scrollama')
+  //   const scrollThreshold = 0.2;
+  //   this.scroller = scrollama()
+  //   this.scroller.setup({
+  //     step: '.timeline-year',
+  //     threshold: scrollThreshold,
+  //     progress: true,
+  //     // debug: true
+  //   })
+  //   .onStepEnter(this.handleScrollStepEnter)
+  //   .onStepExit(this.handleScrollStepExit)
+  //   .onStepProgress(this.handleProgress)
 
-  indicatorClickHandler = (e) => {
-    const id = e.target.dataset.id;
-    this.updateActiveDocumentCard(id);
-  }
+  //   // setup resize event
+  //   window.addEventListener("resize", this.scroller.resize);
+  // }
 
-  updateActiveDocumentCard = (id) => {
-    const documents = this.state.documents;
-    Object.keys(documents).forEach(v => documents[v] = false);
-    documents[id] = true;
-    this.setState({documents});
-  }
+  // componentWillUnmount(){
+  //   this.scroller.destroy();
+  // }
 
-  render () {
+  // const indicatorClickHandler = (e) => {
+  //   const id = e.target.dataset.id;
+  //   this.updateActiveDocumentCard(id);
+  // }
+
+  // const updateActiveDocumentCard = (id) => {
+  //   const documents = this.state.documents;
+  //   Object.keys(documents).forEach(v => documents[v] = false);
+  //   documents[id] = true;
+  //   this.setState({documents});
+  // }
 
   return (
     <Layout>
       <Head title="Methodology"/>
-      <div className="container">
+      <Container>
         <Row className="">
           <Col md="4" xl="3" className="p-4 p-xl-5">
             <h2>interaction</h2>
           </Col>
           <Col md="8" xl="9" className="h-100 p-md-4 p-xl-5">
-            
-            <StaticQuery
-              query = {graphql`
-                query {
-                  site {
-                    siteMetadata {
-                      methodology {
-                        year
-                        headline
-                        copy
-                        docs {
-                          date
-                          author
-                          title
-                          quote
-                        }
-                      }
-                    }
-                  }
-                }
-              `}
-              render = { data => (
-                <div className="timeline-wrapper mr-1 mr-md-5">
-                { data.site.siteMetadata.methodology.map(item => (
-                  <div key={item.year} className="timeline-year mb-5" data-index={item.year}>
-                    <div className="timeline-year-content">
-                      <div className="timeline-year-content-header d-md-flex pb-2 mb-4">
-                        <h1 className="display-3 pr-3"><strong>{item.year}</strong></h1>
-                        <div className="timeline-year-header-meta mt-2 pr-2 pr-md-5 pb-3">
-                          <p className="mb-1"><strong>{item.headline}</strong></p>
-                          <p className="mb-0">{item.copy}</p>
-                        </div>
-                        { item.docs.map((doc, index) => {
-                          const length = item.docs.length;
-                          const offset = (index / length) * 100;
-                          
-                          return (
-                            <div 
-                              key={index} 
-                              className="timeline-card-indicator" 
-                              data-id={`${item.year}-card-${index}`} 
-                              role="button" 
-                              style={{ left: offset + '%'}} 
-                              onKeyDown={ this.indicatorClickHandler } 
-                              onClick={ this.indicatorClickHandler }>
-                                {item.year}-document-{index}
-                            </div>
-                          )
-                        })}
-                      </div>
-                      <div className="timeline-year-docs mr-3 mr-md-5">
-                        { item.docs.map((doc, index) => {
-                          return(
-                            <DocumentCard key={index} index={index} doc={doc} item={item} active={this.state.documents[`${item.year}-card-${index}`]}  />
-                          )
-                        })}
-                      </div>
+            {/*  
+            <div className="timeline-wrapper mr-1 mr-md-5">
+            { data.site.siteMetadata.methodology.map(item => (
+              <div key={item.year} className="timeline-year mb-5" data-index={item.year}>
+                <div className="timeline-year-content">
+                  <div className="timeline-year-content-header d-md-flex pb-2 mb-4">
+                    <h1 className="display-3 pr-3"><strong>{item.year}</strong></h1>
+                    <div className="timeline-year-header-meta mt-2 pr-2 pr-md-5 pb-3">
+                      <p className="mb-1"><strong>{item.headline}</strong></p>
+                      <p className="mb-0">{item.copy}</p>
                     </div>
+                    { item.docs.map((doc, index) => {
+                      const length = item.docs.length;
+                      const offset = (index / length) * 100;
+                            
+                      return (
+                        <div 
+                          key={index} 
+                          className="timeline-card-indicator" 
+                          data-id={`${item.year}-card-${index}`} 
+                          role="button" 
+                          style={{ left: offset + '%'}} 
+                          onKeyDown={ this.indicatorClickHandler } 
+                          onClick={ this.indicatorClickHandler }>
+                            {item.year}-document-{index}
+                        </div>
+                      )
+                    })}
                   </div>
-                ))}
+                  <div className="timeline-year-docs mr-3 mr-md-5">
+                    { item.docs.map((doc, index) => {
+                      return(
+                        <DocumentCard key={index} index={index} doc={doc} item={item} active={this.state.documents[`${item.year}-card-${index}`]}  />
+                      )
+                    })}
+                  </div>
+                </div>
               </div>
-            )}
-          />
-            
-          </Col>
+            ))}
+            </div>  
+            */}
+            </Col>
         </Row>
-      </div>
+      </Container>
     </Layout>
-    // <Layout>
-    //   <Head title="Methodology"/>
-    //   <Container className="mt-4">
-    //   <h1>Methodology</h1> 
-    //   <p>Posts will show up heree.</p>
-    //   { data.allContentfulBlogPost.edges.map(edge => (
-    //     <Link key={edge.node.slug} to={ `/blog/${edge.node.slug}`}>
-    //       <Card className="mb-2">
-    //         <Card.Body>
-    //           <h2>{edge.node.title}</h2>
-    //           <p className="mb-0">{edge.node.publishedDate}</p>
-    //         </Card.Body>
-    //       </Card>
-    //     </Link>
-    //   ))}
-    //   </Container>
-    // </Layout>
   )
-}
 }
 
 export default MethodologyPage
