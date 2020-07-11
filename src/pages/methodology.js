a51763e63cf7482e28b288cc983f301d32de17b2
Joshua Richards <joshua.j.richards@gmail.com>
2020-07-11 12:42:31 -0400

    temp fix methodology

--- src/pages/methodology.js
+++ src/pages/methodology.js
@@ -150,24 +150,7 @@ const MethodologyPage = () => {
                       <p className="mb-0">{item.node.description.description}</p>
                     </div>
                     <div className="timeline-year-buttons">
-                    { item.node.documents.map((doc, index) => {
-                      const length = item.node.documents.length;
-                      const offset = (index / length) * 100;
-                            
-                      return (
-                        <button 
-                          key={index} 
-                          className="timeline-card-indicator" 
-                          data-id={`${item.node.year}-card-${index}`} 
-                          role="button" 
-                          style={{ left: offset + '%'}} 
-                          // onKeyDown={ indicatorClickHandler } 
-                          onClick={ indicatorClickHandler }
-                        >
-                          {item.node.year}-document-{index}
-                        </button>
-                      )
-                    })}
+                    
                     </div>
                     { item.node.documents.map((doc, index) => {
                       const length = item.node.documents.length;
@@ -188,18 +171,7 @@ const MethodologyPage = () => {
                       )
                     })}
                   </div>
-                  <div className="timeline-year-docs mr-3 mr-md-5">
-                    { item.node.documents.map((doc, index) => {
-                      return(
-                        <DocumentCard 
-                          key={index} 
-                          index={index} 
-                          doc={doc} 
-                          item={item.node} 
-                          active={state.documents[`${item.node.year}-card-${index}`]}  />
-                      )
-                    })}
-                  </div>
+                  
                   <div className="timeline-year-docs mr-3 mr-md-5">
                     { item.node.documents.map((doc, index) => {
                       let id = `${item.node.year}-card-${index}`;
