2af036667984b12bdb503728e6c6b34888319409
Maggie <mc@maggiecheung.ca>
2020-07-23 12:57:57 -0400

    Added react-bootstrap tabs

--- src/components/issues-momentum.js
+++ src/components/issues-momentum.js
@@ -1,13 +1,28 @@
 import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
-import { Link } from "gatsby"
+import { Row, Col, Tabs, Tab } from "react-bootstrap"
+// import { Link } from "gatsby"
 import "./issues-momentum.scss"
 
 class MomentumTabs extends React.Component {
   render() {
     return (
       <Row>
-        <p>Momentum Tabs.</p>
+        <Col md="12">
+          <Tabs defaultActiveKey="2017" id="uncontrolled-tab-example">
+            <Tab eventKey="2017" title="2017">
+              <p>Content for the children here.</p>
+            </Tab>
+            <Tab eventKey="2018" title="2018">
+              <p>Content for the children here.</p>
+            </Tab>
+            <Tab eventKey="2019" title="2019">
+              <p>Content for the children here.</p>
+            </Tab>
+            <Tab eventKey="2020" title="2020">
+              <p>Content for the children here.</p>
+            </Tab>
+          </Tabs>
+        </Col>
       </Row>
     )
   }
