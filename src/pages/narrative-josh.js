c5ef438444c4fe9195ef0a5fffc6588abdc3ba37
Joshua Richards <joshua.j.richards@gmail.com>
2020-08-05 21:25:22 -0400

    add scroll-magic

--- src/pages/narrative-josh.js
+++ src/pages/narrative-josh.js
@@ -1,13 +1,27 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import { Controller, Scene } from 'react-scrollmagic';
 
 const NarrativeJoshPage = ({data}) => {
   console.log(data);
-  
-  return (
-    <Layout>
 
+  const sortedData = [...data.allContentfulNarrativePageTemplate.edges];
+
+  return (
+    <Layout>  
+      <Controller>
+        { sortedData.map((page, index) => {
+            return (
+              <Scene key={`slide-${index}`} duration={600} pin>
+                <div className="vh-100">
+                  { page.node.character }
+                </div>
+              </Scene>
+            )
+          }
+        )}
+      </Controller>
     </Layout>
   )
 }
@@ -19,6 +33,7 @@ export const query = graphql`
     allContentfulNarrativePageTemplate {
       edges {
         node {
+          character
           slideNumber
         }
       }
