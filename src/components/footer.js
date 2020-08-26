a3a91add2d850ffb34f070331b1b9f748bcdfc3e
Joshua Richards <joshua.j.richards@gmail.com>
2020-08-26 09:41:24 -0400

    update footer cc

--- src/components/footer.js
+++ src/components/footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
   return (
     <footer className={`bg-dark text-white text-center`}>
       <Container>
-        <p className="m-0 py-3">Created by <a href="https://lawdesigncolab.ca/" title="Law &amp; Design Co-Lab" aria-label="Learn more about the civic tech nonprofit behind this project">{data.site.siteMetadata.author}</a> <a href="https://creativecommons.org/licenses/by-sa/4.0/" title="Creative Commons BY-SA 4.0"</a> 2020</p>
+        <p className="m-0 py-3">Created by <a href="https://lawdesigncolab.ca/" title="Law &amp; Design Co-Lab" aria-label="Learn more about the civic tech nonprofit behind this project">{data.site.siteMetadata.author}</a> <a href="https://creativecommons.org/licenses/by-sa/4.0/" title="Creative Commons BY-SA 4.0">Creative Commons BY-SA 4.0</a> 2020</p>
       </Container>
     </footer>
   )
